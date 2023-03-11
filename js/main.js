987f69c6447b67a593704d812b10c2bc14b3511a
macielaraujo <104031118+macielaraujo@users.noreply.github.com>
2023-03-11 17:17:06 -0300

    concluindo mobile first

--- js/main.js
+++ js/main.js
@@ -2,3 +2,17 @@
 function entrarApp(){
     window.location = 'https://wa.link/vag2cn'
 }
+
+function mostrar(){
+    let botao = document.getElementById('menuItem');
+    if(botao.style.display=='block'){
+        botao.style.display='none'
+    }else{botao.style.display='block'}
+}
+
+function mudaTamanho(){
+    let botao = document.getElementById('menuItem');
+    if(window.innerWidth>=760){
+        botao.style.display='block'
+    }else{botao.style.display='none'}
+}
